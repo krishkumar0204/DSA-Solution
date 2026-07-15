43ce0e2d20399746ab24fe0a9dbf0299ade8ab3a
Krish Kumar <krishsadipur@gmail.com>
2026-07-15 08:08:36 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (75.02%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -0,0 +1,77 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reverse(ListNode head, int size){
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while(size > 0){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            size--;
+        }
+    }
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null || head.next == null){
+            return head;
+        }
+
+
+        ListNode left = head;
+        ListNode prevLeft = null;
+        ListNode res = null;
+        int size = k;
+
+
+        while(left != null){
+            ListNode right = left;
+
+            for(int i = 0 ; i < size - 1; i++){
+                if(right == null){
+                    break;
+                }
+                right = right.next;
+            }
+
+            if(right != null){
+                ListNode nextLeft = right.next;
+
+                reverse(left, size);
+
+                if(res == null){
+                    res = right;
+                }
+
+                if(prevLeft != null){
+                    prevLeft.next = right;
+                }
+                
+                left.next = nextLeft;
+                prevLeft = left;
+                left = nextLeft;
+            }else{
+
+                if(prevLeft != null){
+                    prevLeft.next = left;
+                }
+
+                if(res == null){
+                    res = left;
+                }
+
+                break;
+            }
+        }
+        return res;
+    }
+}
